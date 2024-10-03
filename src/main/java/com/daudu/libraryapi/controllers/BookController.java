db5bb709c3bdd5453331c2b2b4102d3f93a76540
daudujohn <daudujon@gmail.com>
2024-10-03 14:02:03 +0100

    feat: add book controller

---
+++ src/main/java/com/daudu/libraryapi/controllers/BookController.java
@@ -0,0 +1,34 @@
+package com.daudu.libraryapi.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.daudu.libraryapi.domain.dto.BookDto;
+import com.daudu.libraryapi.domain.entities.BookEntity;
+import com.daudu.libraryapi.mappers.Mapper;
+import com.daudu.libraryapi.services.BookService;
+
+@RestController
+public class BookController {
+
+    private Mapper<BookEntity, BookDto> bookMapper;
+
+    private BookService bookService;
+
+    public BookController(Mapper<BookEntity, BookDto> bookMapper, BookService bookService) {
+        this.bookMapper = bookMapper;
+        this.bookService = bookService;
+    }
+
+    @PutMapping(path = "/books/{isbn}")
+    public ResponseEntity<BookDto> createBook(@PathVariable("isbn") String isbn, @RequestBody BookDto bookDto) {
+        BookEntity bookEntity = bookMapper.mapFrom(bookDto);
+        BookEntity savedBookEntity = bookService.createBook(isbn, bookEntity);
+        BookDto savedBookDto = bookMapper.mapTo(savedBookEntity);
+        return new ResponseEntity<>(savedBookDto, HttpStatus.CREATED);
+    }
+}
