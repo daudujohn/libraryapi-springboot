3f48bb21468ce5906b3b4cc4315248308fbd6af0
daudujohn <daudujon@gmail.com>
2024-10-03 12:38:56 +0100

    feat: add author and book repositories

--- src/main/java/com/daudu/libraryapi/repositories/AuthorRepository.java
+++ src/main/java/com/daudu/libraryapi/repositories/AuthorRepository.java
@@ -1,5 +1,11 @@
 package com.daudu.libraryapi.repositories;
 
-public interface AuthorRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.daudu.libraryapi.domain.entities.AuthorEntity;
+
+@Repository
+public interface AuthorRepository extends CrudRepository<AuthorEntity, Long> {
 
 }
--- src/main/java/com/daudu/libraryapi/repositories/BookRepository.java
+++ src/main/java/com/daudu/libraryapi/repositories/BookRepository.java
@@ -1,5 +1,11 @@
 package com.daudu.libraryapi.repositories;
 
-public interface BookRepository {
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.daudu.libraryapi.domain.entities.BookEntity;
+
+@Repository
+public interface BookRepository extends CrudRepository<BookEntity, String> {
 
 }
