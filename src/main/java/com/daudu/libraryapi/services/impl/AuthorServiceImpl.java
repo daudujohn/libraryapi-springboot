b5966122ab82dee1fe16b023478a256b55fea267
daudujohn <daudujon@gmail.com>
2024-10-03 18:48:17 +0100

    feat: add delete author service

--- src/main/java/com/daudu/libraryapi/services/impl/AuthorServiceImpl.java
+++ src/main/java/com/daudu/libraryapi/services/impl/AuthorServiceImpl.java
@@ -53,5 +53,10 @@ public class AuthorServiceImpl implements AuthorService {
             return authorRepository.save(existingAuthor);
         }).orElseThrow(() -> new RuntimeException("Author does not exist"));
     }
-    
+
+    @Override
+    public void delete(Long id) {
+        authorRepository.deleteById(id);
+    }
+
 }
