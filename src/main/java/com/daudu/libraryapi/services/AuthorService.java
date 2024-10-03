9f69d2edc8e1a957d1c7195e171c38af666033d9
daudujohn <daudujon@gmail.com>
2024-10-03 14:31:09 +0100

    feat: add find all authors

--- src/main/java/com/daudu/libraryapi/services/AuthorService.java
+++ src/main/java/com/daudu/libraryapi/services/AuthorService.java
@@ -1,7 +1,11 @@
 package com.daudu.libraryapi.services;
 
+import java.util.List;
+
 import com.daudu.libraryapi.domain.entities.AuthorEntity;
 
 public interface AuthorService {
     AuthorEntity createAuthor(AuthorEntity authorEntity);
+
+    List<AuthorEntity> findAll();
 }
--- src/main/java/com/daudu/libraryapi/services/impl/AuthorServiceImpl.java
+++ src/main/java/com/daudu/libraryapi/services/impl/AuthorServiceImpl.java
@@ -1,5 +1,9 @@
 package com.daudu.libraryapi.services.impl;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
 import org.springframework.stereotype.Service;
 
 import com.daudu.libraryapi.domain.entities.AuthorEntity;
@@ -18,5 +22,13 @@ public class AuthorServiceImpl implements AuthorService {
     @Override
     public AuthorEntity createAuthor(AuthorEntity authorEntity) {
         return authorRepository.save(authorEntity);
+    }
+
+    @Override
+    public List<AuthorEntity> findAll() {
+        return StreamSupport.stream(authorRepository
+                .findAll()
+                .spliterator(), false)
+                .collect(Collectors.toList());
     }
 }
