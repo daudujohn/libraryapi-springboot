685bb8664db6c95ae881dc75c9782139a239a1cf
daudujohn <daudujon@gmail.com>
2024-10-04 14:42:22 +0100

    feat: add user repository

---
+++ src/main/java/com/daudu/libraryapi/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+
+package com.daudu.libraryapi.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import com.daudu.libraryapi.domain.entities.UserEntity;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long>,
+        PagingAndSortingRepository<UserEntity, Long> {
+
+}
