f353d9363ee90184aad5f15af43691f04eb8eff0
daudujohn <daudujon@gmail.com>
2024-10-07 10:22:06 +0100

    feat: add query by email

--- src/main/java/com/daudu/libraryapi/repositories/UserRepository.java
+++ src/main/java/com/daudu/libraryapi/repositories/UserRepository.java
@@ -1,6 +1,8 @@
 
 package com.daudu.libraryapi.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -10,5 +12,6 @@ import com.daudu.libraryapi.domain.entities.UserEntity;
 @Repository
 public interface UserRepository extends CrudRepository<UserEntity, Long>,
         PagingAndSortingRepository<UserEntity, Long> {
+        Optional<UserEntity> findByEmail(String email);
 
 }
