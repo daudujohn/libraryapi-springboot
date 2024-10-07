a9c9a7ef79b5d2aa8aacafa005e2c794bf2cee55
daudujohn <daudujon@gmail.com>
2024-10-07 10:28:36 +0100

    feat: add user principal

---
+++ src/main/java/com/daudu/libraryapi/domain/entities/UserPrincipal.java
@@ -0,0 +1,32 @@
+package com.daudu.libraryapi.domain.entities;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserPrincipal implements UserDetails {
+
+    private UserEntity userEntity;
+
+    public UserPrincipal(UserEntity userEntity) {
+        this.userEntity = userEntity;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(new SimpleGrantedAuthority("USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return userEntity.getEmail();
+    }
+
+    @Override
+    public String getPassword() {
+        return userEntity.getPassword();
+    }
+}
